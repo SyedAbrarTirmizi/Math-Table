7d0ed796164605fd9a707256abd736a899a4ca9a
Syed Abrar Tirmiz <sabrar5000@gmail.com>
2024-04-20 04:35:01 +0500

    Table Generator

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
-let getData = document.querySelector("#number");
-let putData = document.querySelector("#display");
-let inputForm = document.querySelector("#table-form");
-
-inputForm.addEventListener("submit", (event) => {
+document.getElementById("table").addEventListener("click", function (event) {
   event.preventDefault();
-  let tableOf = input.value;
-
-  display.innerHTML = "";
-
-  for (let i = 1; i <= 10; i++) {
-    const msg = `${tableOf} X ${i} = ${tableOf * i}`;
-    display.innerHTML += `${msg} <br>`;
+  let getData = document.getElementById("number").value;
+  let putData = document.getElementById("display");
+  putData.innerHTML = "";
+  for (i = 1; i <= 10; i++) {
+    putData.innerHTML += getData + " X " + i + " = " + getData * i + "<br>";
   }
 });
